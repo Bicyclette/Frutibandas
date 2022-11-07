980530ca8639bea5f9777d0a828e061cf2bda5a2
Bicyclette <mathias.velo@gmail.com>
2022-11-08 00:19:52 +0100

    added server @:PORT

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@
 #include "editorUI.hpp"
 #include "allocation.hpp"
 
-#define SERVER "127.0.0.1"
-#define PORT 7777
+#define SERVER "staging.frutibandas.eternaltwin.org"
+#define PORT 50385
 
 void connect(std::shared_ptr<Game>& game)
 {
