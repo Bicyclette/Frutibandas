8b045cae8e8aa99da9e34ced4a1b82644dd2db36
Bicyclette <mathias.velo@gmail.com>
2022-11-08 12:16:43 +0100

    staging

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,10 @@
 #include "editorUI.hpp"
 #include "allocation.hpp"
 
-//#define SERVER "staging.frutibandas.eternaltwin.org"
-//#define PORT 50385
-#define SERVER "127.0.0.1"
-#define PORT 7777
+#define SERVER "staging.frutibandas.eternaltwin.org"
+#define PORT 50385
+//#define SERVER "127.0.0.1"
+//#define PORT 7777
 
 void connect(std::shared_ptr<Game>& game)
 {
